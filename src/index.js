81894fa060c7449318d999efe337a62e27d18ae3
ITigor1985 <xromer85@gmail.com>
2021-12-03 08:49:16 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-// import Notiflix from 'notiflix';
-// import axios from 'axios';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import './sass/main.scss';
